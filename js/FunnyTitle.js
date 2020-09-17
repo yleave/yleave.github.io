78ad247b370d841ac392a575e07b32d8b9b7a655
yleave <38160787+yleave@users.noreply.github.com>
2020-09-17 15:26:14 +0000

    Deploying to master from  @ e4cc14fe05b82429ca63db7ab8e9c019464ac2a6 🚀

---
+++ js/FunnyTitle.js
@@ -0,0 +1,17 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "http://qgt3t1irs.hd-bkt.clouddn.com/logo1.png");
+        document.title = '╭(°A°`)╮ 你怎么走啦 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "http://qgt3t1irs.hd-bkt.clouddn.com/logo1.png");
+        document.title = '(ฅ>ω<*ฅ) 噫你回来啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});

556de92db4047ae3a97639d574781be540466310
yleave <38160787+yleave@users.noreply.github.com>
2020-09-20 04:04:33 +0000

    Deploying to master from  @ 01e68862aaf48cfb87e5c63e8c672b81ff63eef9 🚀

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "http://qgt3t1irs.hd-bkt.clouddn.com/logo1.png");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/yleave/imagehost/img/logo1.png");
         document.title = '╭(°A°`)╮ 你怎么走啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "http://qgt3t1irs.hd-bkt.clouddn.com/logo1.png");
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/yleave/imagehost/img/logo1.png");
         document.title = '(ฅ>ω<*ฅ) 噫你回来啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
